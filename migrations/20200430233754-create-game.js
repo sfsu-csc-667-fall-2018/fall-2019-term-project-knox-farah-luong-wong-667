33f2a1611c464595e3ea0a5e841c6fe51426f752
dylan <dluong1@mail.sfsu.edu>
2020-04-30 20:42:30 -0700

    added game table to db, may need some alteration later

---
+++ migrations/20200430233754-create-game.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+ up: (queryInterface, Sequelize) => {
+ return queryInterface.createTable(
+ 'Games',
+ {
+ gid: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+ },
+ hostUid: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {model: 'Users', key: 'id'} 
+ },
+ guestUid: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    references: {model: 'Users', key: 'id'}
+ },
+ createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.literal('NOW()'),
+    allowNull: false
+ }
+ }
+ );
+ },
+ down: (queryInterface, Sequelize) => {
+ return queryInterface.dropTable('game');
+ }
+};
