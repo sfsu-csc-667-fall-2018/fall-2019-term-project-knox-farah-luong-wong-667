4e36024765c7ed0dc84bd2124c4f9cf9b03ef79e
Aramis Knox <carbon.moknoxide@gmail.com>
2020-05-02 08:02:18 -0700

    Connected frontend signup and reg with backend api

--- routes/api/user.js
+++ routes/api/user.js
@@ -7,35 +7,6 @@ const db = require("../../db/index");
 const UserModel = require("../../models/user");
 const User = UserModel(db, Sequelize);
 
-router.get("/createUser", function (request, response, next) {
-    db.authenticate()
-      .then(() => console.log('Database connected...'))
-      .then(function() {
-        User.create({
-          email: 'test01@gmail.com',
-          password: 'password',
-          username: 'cool_username'
-        })
-      })
-      .then(User.findAll())
-      .then((results) => response.json(results))
-      .catch(err => console.log('Error: ' + err))
-  });
-  
-  router.get("/getUsers", function (request, response, next) {
-    db.authenticate()
-      .then(() => console.log('Database connected...'))
-      .then(_ => User.findAll())
-      .then((results) => response.json(results))
-      .catch(err => console.log('Error: ' + err))
-  });
-
-  router.post("/display", function (request, response, next) {
-      db.authenticate()
-        .then(console.log("Got body: ", request.body.emailsignup))
-        .then(_ => response.send("Response"))
-  });
-
   router.post("/create", function (request, response, next) {
       bcrypt.hash(request.body.passwordsignup, saltRounds, function (err, hash) {
           User.create({
@@ -61,11 +32,9 @@ router.get("/createUser", function (request, response, next) {
           } else {
               bcrypt.compare(request.body.password, user.password, function (err, result) {
                   if (result == true) {
-                      response.json(user)
-                      response.redirect('/');
+                      response.send(user);
                   } else {
                       response.send('Incorrect password');
-                      response.redirect('/');
                   }
               });
           }
