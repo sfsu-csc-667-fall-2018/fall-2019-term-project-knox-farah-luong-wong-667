ea9972bf3e47c6d626ef2723282fe81c3a21b26d
Mohamed <farahmo242@gmail.com>
2020-05-04 14:47:42 -0700

    Fixed login/Registration page

--- routes/api/user.js
+++ routes/api/user.js
@@ -7,38 +7,38 @@ const db = require("../../db/index");
 const UserModel = require("../../models/user");
 const User = UserModel(db, Sequelize);
 
-  router.post("/create", function (request, response, next) {
-      bcrypt.hash(request.body.passwordsignup, saltRounds, function (err, hash) {
-          User.create({
-              email: request.body.emailsignup,
-              password: hash,
-              username: request.body.usernamesignup
-          }).then(function(data) {
-              if(data) {
-                  response.redirect('/');
-              }
-          });
-      });
-  });
+router.post("/create", function (request, response, next) {
+    bcrypt.hash(request.body.passwordsignup, saltRounds, function (err, hash) {
+        User.create({
+            email: request.body.emailsignup,
+            password: hash,
+            username: request.body.usernamesignup
+        }).then(function (data) {
+            if (data) {
+                response.redirect('/');
+            }
+        });
+    });
+});
 
-  router.post('/verify', function (request, response, next) {
-      User.findOne({
-          where: {
-              email: request.body.email
-          }
-      }).then(function(user) {
-          if(!user) {
-              response.redirect('/');
-          } else {
-              bcrypt.compare(request.body.password, user.password, function (err, result) {
-                  if (result == true) {
-                      response.send(user);
-                  } else {
-                      response.send('Incorrect password');
-                  }
-              });
-          }
-      });
-  });
+router.post('/verify', function (request, response, next) {
+    User.findOne({
+        where: {
+            email: request.body.email
+        }
+    }).then(function (user) {
+        if (!user) {
+            response.redirect('/');
+        } else {
+            bcrypt.compare(request.body.password, user.password, function (err, result) {
+                if (result == true) {
+                    response.send(user);
+                } else {
+                    response.send('Incorrect password');
+                }
+            });
+        }
+    });
+});
 
-  module.exports = router;
+module.exports = router;
