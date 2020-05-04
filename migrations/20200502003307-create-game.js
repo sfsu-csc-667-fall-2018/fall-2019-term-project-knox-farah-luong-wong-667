fc6721d9c481a6046d7de312dd0949c6ac59da0b
Dylan Luong <dluong1@mail.sfsu.edu>
2020-05-04 16:39:02 -0700

    fixed game api

--- migrations/20200502003307-create-game.js
+++ migrations/20200502003307-create-game.js
@@ -8,6 +8,23 @@ module.exports = {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4
       },
+      hostUid: {
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      guestUid: {
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- routes/api/game.js
+++ routes/api/game.js
@@ -11,26 +11,62 @@ router.get('/', (request, response, next) => {
 });
   
 
-router.get('/createGame', (request,response,next)=>{
-  db.authenticate()
-    .then(() => console.log('Database connected...'))
-    .then(function() {
-      Game.create({
-        hostUid: '1',
-        guestUid: '2'
-      })
-    })
-    .then(() => console.log('Successfully created a game.'))
-    .then(_ => response.json('Successfully created a game.'))
-    .catch(
-      error => { 
-        console.log(error)
-        response.json({ error })
-      }
-    )
+//test: creates a game with two pre-determined Uids.
+router.post('/test/create', (request,response,next)=>{
+  Game.create({
+    hostUid: "4e576e8b-da82-48d9-af72-3d3310f6518b",
+    guestUid: "b4631842-ed7e-4a90-a7bf-93e6e95a479c",
+  })
+  .then((result) => {
+    response.json(result)
+  })
 });
 
-router.get('/getGames',(request,response,next) => {
+//creates a game with only host
+//format: ../api/game/create/some host id
+router.post('/create/:hUid/', (request,response,next)=>{
+  const { hUid } = request.params;
+  Game.create({
+    hostUid: hUid,
+  })
+  .then((result) => {
+    response.json(result)
+  })
+});
+
+//creates a game with host and guest
+//format: ../api/game/create/some host id/some guest id
+router.post('/create/:hUid/:gUid', (request,response,next)=>{
+  const { hUid } = request.params;    
+  const { gUid } = request.params;
+  Game.create({
+    hostUid: hUid,
+    guestUid: gUid,
+  })
+  .then((result) => {
+    response.json(result)
+  })
+});
+
+//creates multiple games with only host 
+//format: ../api/game/createbulk/some host id/
+router.post("/createbulk/:hUid", (request, response, next) => {
+  const { hUid } = request.params;
+  Game.bulkCreate([
+    { hostUid: hUid},
+    { hostUid: hUid},
+    { hostUid: hUid},
+])
+.then((results) =>{
+    response.json(results)
+})
+.catch((err)=>{
+    response.json(err)
+})
+});
+
+//shows all games
+router.get('/getAll',(request,response,next) => {
   db.authenticate()
     .then(() => console.log('Database connected...'))
     .then(_=>Game.findAll())
