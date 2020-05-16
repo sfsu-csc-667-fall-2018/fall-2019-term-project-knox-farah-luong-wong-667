01dae711bce0f8f37843412c09ceba67ec153602
Dylan Luong <dluong1@mail.sfsu.edu>
2020-05-15 18:04:17 -0700

    added tile tray and does show user's piece for the predetermined game

--- routes/games.js
+++ routes/games.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const models = require("../models/associations");
 const User = models["User"];
+const Tile = models["Tile"];
 
 // Needs work/discuss this
 // router.get("/", (request, response, next) => {
@@ -12,6 +13,26 @@ const User = models["User"];
 // });
 
 router.get("/", (request, response, next) => {
-    response.render("authenticated/game",{ username: request.session.username })
+  const playerhand = []
+  response.render("authenticated/game",{ username: request.session.username, playerhand: playerhand })
 });
+
+//returns player's hand 
+//game id is pre-determined for now
+router.get("/test", (request, response, next) => {
+    gid = "1fab3405-4142-42be-8881-386359524428"
+    Tile.findAll({
+        where: {
+          GameId: gid,
+          UserId: request.session.uid,
+          xCoordinate: null,
+          yCoordinate: null
+        }
+      })
+      .then((result) => {
+        console.log(result)
+        response.render("authenticated/game",{ username: request.session.username , playerhand: result})
+    })
+})
+
 module.exports = router;
