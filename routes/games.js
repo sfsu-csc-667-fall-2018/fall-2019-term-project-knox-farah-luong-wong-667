46ff7c0c93f749a895e9cafc93feaf0b78ccb877
Aramis Knox <carbon.moknoxide@gmail.com>
2020-05-16 07:39:13 -0700

    implemented game setup on join/create

--- routes/games.js
+++ routes/games.js
@@ -1,17 +1,29 @@
+const Sequelize = require("sequelize");
 const express = require("express");
 const router = express.Router();
 const models = require("../models/associations");
 const User = models["User"];
+const Tile = models["Tile"];
+const Game = models["Game"];
 
-// Needs work/discuss this
-// router.get("/", (request, response, next) => {
-//   response.render("layout", {
-//     text: "Implement authentication, should have an id",
-//     condition: false,
-//   });
-// });
 
 router.get("/", (request, response, next) => {
-    response.render("authenticated/game",{ username: request.session.username })
-});
+  Tile.findAll({
+    where: {
+      GameId: request.session.gid,
+      UserId: request.session.uid,
+      xCoordinate: null,
+      yCoordinate: null
+    }
+  })
+  .then((result) => {
+    if(result.length < 7) {
+      response.redirect("/api/game/fillPlayerHand")
+    } else {
+      console.log(result)
+      response.render("../views/authenticated/game", { username: request.session.username, playerHand: result })
+    }
+  })
+})
+    
 module.exports = router;
