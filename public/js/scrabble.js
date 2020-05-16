491dc25d542a863a27495dd3c7fd27e98ddcda34
Aramis Knox <carbon.moknoxide@gmail.com>
2020-05-16 14:26:48 -0700

    disabled selection of filled board coordinates

--- public/js/scrabble.js
+++ public/js/scrabble.js
@@ -1,5 +1,15 @@
 const container = document.getElementById("con");
 
+var selectedX = -1
+var selectedY = -1
+//I have:
+//Hand and board
+//If I click on board I want it to stay highighted
+//If I click on a tile in the hand I want it to stay highlighted
+//If there is a selected board square and it is empty place the tile in the hand there
+//If it isn't empty, do not highlight the tile
+//Remove the tile from 
+
 function fillTable(grid) {
   console.log(grid)
   document.body.style.setProperty("--grid-rows", grid.length);
@@ -9,7 +19,14 @@ function fillTable(grid) {
       let cell = document.createElement("div")
       cell.setAttribute('x', i)
       cell.setAttribute('y', j)
-      cell.onclick = function() { alert(cell.getAttribute('x')); };
+      cell.onclick = function() {
+        console.log("Inner HTML: ", cell.innerHTML)
+        if(cell.innerHTML === "") {
+          selectedX = cell.getAttribute('x') 
+          selectedY = cell.getAttribute('y')
+          console.log("Selected X: ", selectedX, "| Selected Y: ", selectedY)
+        }
+      }; //What do I want to happen here?
       if(grid[i][j] == null) {
         container.appendChild(cell).className += " grid-item";
       } else {
