d44bc19cfaeec977f768ebd42998eefac657531b
Aramis Knox <carbon.moknoxide@gmail.com>
2020-05-16 17:27:58 -0700

    intermediate commit - naive score tracking implemented

--- public/js/scrabble.js
+++ public/js/scrabble.js
@@ -5,6 +5,38 @@ var playerHand = -1
 var gameBoard = -1
 var gameData = -1
 var selectedPiece = -1
+var turnScore = 0
+
+
+const scores = {
+  ' ': 0,
+  'A': 1,
+  'E': 1,
+  'I': 1,
+  'O': 1,
+  'N': 1,
+  'R': 1,
+  'T': 1,
+  'L': 1,
+  'S': 1,
+  'U': 1,
+  'D': 2,
+  'G': 2,
+  'B': 3,
+  'C': 3,
+  'M': 3,
+  'P': 3,
+  'F': 4,
+  'H': 4,
+  'V': 4,
+  'W': 4,
+  'Y': 4,
+  'K': 5,
+  'J': 8,
+  'X': 8,
+  'Q': 10,
+  'Z': 10
+}
 
 
 function fillTable(grid) {
@@ -52,12 +84,24 @@ function updateGameState(cell) {
   if(cell.innerHTML === "") {
     if (selectedPiece != -1) {
       placeTile(cell)
+      updateScore(cell.innerHTML, true)
     }
   } else {
     if (selectedPieces.includes(cell.getAttribute('tid'))) {
+      updateScore(cell.innerHTML, false)
       replaceToHand(cell)
     }
   }
+}
+
+
+function updateScore(letter, isIncreasing) {
+  if(isIncreasing) {
+    turnScore = turnScore + scores[letter]
+  } else {
+    turnScore = turnScore - scores[letter]
+  }
+  console.log("Turn Score: ", turnScore)
 }
 
 
